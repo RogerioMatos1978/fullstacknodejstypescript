4509bfa95738e78afd11b349be978ffbea5cf0d8
Rogerio Matos <rs_matos78@outlook.com>
2025-05-06 20:20:12 -0300

    Criando variaveis

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,20 @@
 "use strict";
+// Criar variavel do tipo String
 const message = "Ola Mundo";
 console.log(message);
+// Criar variavel do tipo Number
 const idade = 47;
 console.log(idade);
+// Criar variavel do tipo String com format docString ${}
 const user = "Rogerio";
 console.log(`Usuario: ${user}`);
+// Criar variavel do tipo boolean
+const situation = true;
+console.log(`Situação: ${situation}`);
+// Criar variavel do tipo array do tipo string
+const courses = ["Curso de React", "Curso de Node.js"];
+console.log(`Cursos: ${courses}`);
+// Criar variavel do tipo array  do tipo number
+const amout = [1, 2, 3, 4, 6, 10];
+console.log(`Numeros: ${amout}`);
+// Criar variavel do tipo "tuple" array com tipos diferentes
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
+// Criar variavel do tipo String
 const message: string = "Ola Mundo";
 console.log(message);
-
+// Criar variavel do tipo Number
 const idade: number = 47;
 console.log(idade);
-
+// Criar variavel do tipo String com format docString ${}
 const user: string = "Rogerio";
 console.log(`Usuario: ${user}`);
+// Criar variavel do tipo boolean
+const situation: boolean = true;
+console.log(`Situação: ${situation}`);
+// Criar variavel do tipo array do tipo string
+const courses: string[] = ["Curso de React", "Curso de Node.js"];
+console.log(`Cursos: ${courses}`);
+// Criar variavel do tipo array  do tipo number
+const amout: number[] = [1,2,3,4,6,10];
+console.log(`Numeros: ${amout}`);
+// Criar variavel do tipo "tuple" array com tipos diferentes
