af261f502d8b415763ed439db06123d54d8a2926
ROGERIO DA SILVA MATOS <rogeriosilva.sesi@fieg.com.br>
2025-05-06 15:07:10 -0300

    testando tipos de dados no primeiro typeScript NodeJs

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,5 @@
 "use strict";
 const message = "Ola Mundo";
 console.log(message);
+const idade = 47;
+console.log(idade);
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 const message: string = "Ola Mundo";
 console.log(message);
+
+const idade: number = 47;
+console.log(idade);
