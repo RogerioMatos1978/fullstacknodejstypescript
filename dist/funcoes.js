f94e520c3eac948024a7d92e159ad71cd9f1fa8c
Rogerio Matos <rs_matos78@outlook.com>
2025-05-10 09:21:55 -0300

    criando a primeira função para string

--- dist/funcoes.js
+++ dist/funcoes.js
@@ -1,2 +1,8 @@
 "use strict";
 console.log("Como criar funções");
+// Criar a função para receber duas variaveis do tipo string e retornar a string
+function getUser(nome, ultimoNome) {
+    return `${nome} ${ultimoNome}`;
+}
+// Chamar a função getUser e imprimir o valor
+console.log(getUser("Rogerio", "Matos"));
--- src/funcoes.ts
+++ src/funcoes.ts
@@ -1,1 +1,8 @@
 console.log("Como criar funções");
+
+// Criar a função para receber duas variaveis do tipo string e retornar a string
+function getUser(nome: string, ultimoNome: string): string{
+    return `${nome} ${ultimoNome}`;
+}
+// Chamar a função getUser e imprimir o valor
+console.log(getUser("Rogerio", "Matos"));
